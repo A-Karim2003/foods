cd350562b6e7ec23abfe4efbb76fa855ed2b31d5
Abdoulkarim <17a.karim@gmail.com>
2025-04-12 14:29:15 +0100

    Added feature for automatic animation change

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const icons = document.querySelectorAll(".section-1-icons i");
+
+//? add functionality for automatic icon change
+let i = 0;
+setInterval(() => {
+  icons[i].classList.remove("change");
+  i = (i + 1) % icons.length;
+  icons[i].classList.add("change");
+}, 3000);
